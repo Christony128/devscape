a6ba148588396d24ce713a24ccc17e852011b4cc
chris <christony8000@gmail.com>
2026-03-19 14:45:46 +0530

    Created basic http server

---
+++ main.go
@@ -0,0 +1,13 @@
+//basic http server
+package main
+import (
+	"fmt"
+	"net/http"
+)
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Welcome to the API!")
+	})
+	fmt.Println("🚀 Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
